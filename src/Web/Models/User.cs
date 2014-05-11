7b4c2de50618722230dc32e09436a5446180b1cc
Olavo <olavo.castro@itausolucoes.com.br>
2014-05-11 01:19:31 -0300

    opcao de adicionar a permissao de admin

--- src/Web/Models/User.cs
+++ src/Web/Models/User.cs
@@ -1,4 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using BigBallz.Core.Helper;
 
 namespace BigBallz.Models
 {
@@ -13,6 +15,16 @@ namespace BigBallz.Models
             [Required(ErrorMessage = "E-Mail é obrigatório")]
             [EmailAttribute(ErrorMessage = "Informe um E-Mail válido")]
             public string EmailAddress { get; set; }
+        }
+
+        private bool? _isAdmin;
+        public bool IsAdmin
+        {
+            get
+            {
+                return (bool) (_isAdmin = _isAdmin ?? UserRoles.Any(x => x.Role.Name.ToLower() == "admin"));
+            }
+            set { _isAdmin = value; }
         }
     }
 }
